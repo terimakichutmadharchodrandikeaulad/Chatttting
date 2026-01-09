0df43c222edabce63749d754f2c8884ec161b08c
terimakichutmadharchodrandikeaulad <163394065+terimakichutmadharchodrandikeaulad@users.noreply.github.com>
2026-01-09 07:30:31 +0530

    Update API credentials and group/channel names

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "23562992"))
+API_HASH = getenv("API_HASH", "e070a310ca3e76ebc044146b9829237c")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 OWNER_ID = int(getenv("OWNER_ID", None))
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "FRIENDSHIP_CHAT_GROUP0")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "LOVE_ME_BABBY")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "@virat_0011")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "narzofamily")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "narzoxbot")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "@Zolvit")
 
 # Random Start Images
 IMG = [
